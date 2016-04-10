b673c2473c96e0a6d8637562dca89b91d07f1896
tsetimmy <timmy.tse1@gmail.com>
2016-04-10 19:46:22 -0400

    Started working on part 1

--- valueIterationAgents.py
+++ valueIterationAgents.py
@@ -27,8 +27,52 @@ class ValueIterationAgent(ValueEstimationAgent):
     self.discount = discount
     self.iterations = iterations
     self.values = util.Counter() # A Counter is a dict with default 0
-     
+
+
+
+
     "*** YOUR CODE HERE ***"
+    for i in range(len(mdp.getStates())):
+        self.values[mdp.getStates()[i]] = 0.0
+
+    values_tmp = self.values
+
+
+    #exclusion = ['TERMINAL_STATE', (3, 2), (3, 1)]
+    exclusion = []
+    for it in range(self.iterations):
+        for i in range(len(mdp.getStates())):
+            if mdp.getStates()[i] in exclusion:
+                continue
+            actions = mdp.getPossibleActions(mdp.getStates()[i])
+            V_s = []
+            for j in range(len(actions)):
+                summation = 0.0
+                next_states = mdp.getTransitionStatesAndProbs(mdp.getStates()[i], actions[j])
+
+                for ns in next_states:
+                    summation += ns[1] * (mdp.getReward(mdp.getStates()[i], actions[j], ns[0]) + self.discount * self.values[ns[0]])
+                    if mdp.getReward(mdp.getStates()[i], actions[j], ns[0]) != 0.0:
+                        print "hererer"
+                V_s.append(summation)
+
+
+
+
+            #assert(len(V_s) > 0)
+            maximum = V_s[0]
+            for k in range(len(V_s)):
+                maximum = max(maximum, V_s[k])
+            values_tmp[mdp.getStates()[i]] = maximum
+        self.values = values_tmp
+
+
+
+    "*** YOUR CODE HERE -END- ***"
+
+
+
+
     
   def getValue(self, state):
     """
